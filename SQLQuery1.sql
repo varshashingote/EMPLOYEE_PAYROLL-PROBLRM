142281bde89de92132043110e2904a4ba72e7763
varsha Gunjal <varshashingote01@gmail.com>
2023-03-03 07:38:36 +0530

    [varsha]Table is created

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,3 +3,9 @@
 create  database PayRollService248
 use PayRollService248
 
+------create Table---UC2-----------
+create table Employee_PayRoll(id int identity(1,1) primary key,
+Name varchar(300),
+Salary float,
+StartDate date
+)
